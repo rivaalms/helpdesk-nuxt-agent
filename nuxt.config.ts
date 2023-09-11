3c17aed61ac5c624dad3cefffb362ef8963bb38f
rivaalms <rvalms@icloud.com>
2023-09-11 07:28:31 +0700

    chore: set up config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,37 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false }
+	devtools: { enabled: false },
+
+	app: {
+		head: {
+			titleTemplate: "%s | Helpdesk Agent",
+			htmlAttrs: {
+				lang: "en",
+			},
+			meta: [
+				{ name: "viewport", content: "width=device-width, initial-scale=1" },
+				{ hid: "description", name: "description", content: "" },
+				{ name: "format-detection", content: "telephone=no" },
+				{ charset: "utf-8" },
+			],
+		},
+	},
+
+	ssr: false,
+
+	devServer: {
+		port: 4848,
+	},
+
+	runtimeConfig: {
+		public: {
+			apiUrlBase: "",
+			apiUrlPrefix: "",
+		},
+	},
+
+	vite: {
+		define: {
+			"process.env.DEBUG": false,
+		},
+	},
 })
