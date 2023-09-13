b43e7f75c66546d20c4b44cf19a2fad6dbb122d4
rivaalms <rvalms@icloud.com>
2023-09-13 21:29:40 +0700

    feat(header): add dropdown action buttons

--- components/app/header.vue
+++ components/app/header.vue
@@ -4,9 +4,22 @@
       <span>
          Helpdesk
       </span>
-      <span>
-         {{ user.name }}
-      </span>
+      <u-dropdown
+         :items="menu"
+      >
+         <u-button
+            variant="ghost"
+            trailing-icon="i-heroicons-chevron-down-20-solid"
+         >
+            {{ user.name }}
+         </u-button>
+
+         <template #logout="{ item }">
+            <span class="text-red-500">
+               {{ item.label }}
+            </span>
+         </template>
+      </u-dropdown>
    </div>
 </header>
 </template>
@@ -15,4 +28,23 @@
 const authStore = useAuthStore()
 
 const user = authStore.getUser
+
+const menu = [
+   [
+      {
+      label: 'Profile',
+      click: () => { console.log('profile') }
+      },
+   ],
+   [
+      {
+         label: 'Log out',
+         slot: 'logout',
+         click: async () => {
+            await authStore.logout()
+               .then(() => {navigateTo('/login')})
+         }
+      }
+   ]
+]
 </script>
