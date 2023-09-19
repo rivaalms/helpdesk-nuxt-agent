a88e8ca55774535b75b15e5d8371a9625a55125b
rivaalms <rvalms@icloud.com>
2023-09-19 20:49:40 +0700

    feat: app color mode

--- components/app/header.vue
+++ components/app/header.vue
@@ -4,22 +4,35 @@
       <span>
          Helpdesk
       </span>
-      <u-dropdown
-         :items="menu"
-      >
-         <u-button
-            variant="ghost"
-            trailing-icon="i-heroicons-chevron-down-20-solid"
-         >
-            {{ user.name }}
-         </u-button>
+      <div class="flex items-center gap-2">
+         <u-tooltip text="Color mode">
+            <u-button
+               :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+               color="gray"
+               variant="ghost"
+               aria-label="Theme"
+               @click="isDark = !isDark"
+            ></u-button>
+         </u-tooltip>
 
-         <template #logout="{ item }">
-            <span class="text-red-500">
-               {{ item.label }}
-            </span>
-         </template>
-      </u-dropdown>
+         <u-dropdown
+            :items="menu"
+         >
+            <u-button
+               color="gray"
+               variant="ghost"
+               trailing-icon="i-heroicons-chevron-down-20-solid"
+            >
+               {{ user.name }}
+            </u-button>
+   
+            <template #logout="{ item }">
+               <span class="text-red-500">
+                  {{ item.label }}
+               </span>
+            </template>
+         </u-dropdown>
+      </div>
    </div>
 </header>
 </template>
@@ -28,6 +41,15 @@
 const authStore = useAuthStore()
 
 const user = authStore.getUser
+const colorMode = useColorMode()
+const isDark = computed({
+   get () {
+      return colorMode.value === 'dark'
+   },
+   set () {
+      colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+   }
+})
 
 const menu = [
    [
